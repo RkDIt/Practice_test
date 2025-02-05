b562cd06a6bfd285c3906684d236668f1db27829
RkDit <ritikwins@gmail.com>
2025-02-05 13:43:25 +0530

    New Directories

---
+++ 5/2/classes.js
@@ -0,0 +1,49 @@
+
+class Animal{
+    constructor(name){
+        this.name = Animal.capital(name);
+    }
+
+    speak(){
+        console.log(`${this.name} makes noise`)
+    }
+
+    static capital(name){
+        return name.charAt(0).toUpperCase() + name.substring(1, name.length)
+    }
+}
+
+class Birds{
+    #name = " ";
+    #flight= " ";
+    constructor(name, flight){
+        this.#name = name;
+        this.#flight = flight;
+    }
+
+    def() {
+        console.log(`${this.name} ${this.flight} fly`);
+    }
+    //accessing private properties through getter
+    getname(){
+        console.log(this.#name);
+    }
+
+
+}
+
+const c= new Birds("Penguin", "Can not");
+const d = new Birds("Crow", "Can ");
+//access private property>>
+d.getname();
+
+
+const a = new Animal("dog");
+const b = new Animal("cat");
+a.speak();
+b.speak();
+
+
+
+
+
