f6a0c36a45058cb1f9b59d2e1478e180861caba4
RkDit <ritikwins@gmail.com>
2025-02-11 10:35:18 +0530

    updated

--- 11_feb/asssignment.js
+++ 11_feb/asssignment.js
@@ -1,6 +1,20 @@
 const arr = [12, 5, 8, 7, 15, 4, 20, 1];
 const arr_even= [];
 const arr_odd= [];
+const arr_indi = [];
+
+for(let i=0; i<=arr.length-1;i++){
+    
+    if(i %2 ==0){
+            arr_indi.push(arr[i])
+        }
+    if(arr[i]%2 == 0 ){
+        arr_even.push(arr[i])
+    }
+    else{
+        arr_odd.push(arr[i])
+    }
+}
 
 
 function sum(arr){
@@ -12,29 +26,18 @@ for(let i= 1; i<=arr.length-1; i++){
 return total;
 }
 
+
 function product(arr){
     let prod = arr[0];
     
     for(let i = 1; i<=arr.length -1; i++){
         prod  *= arr[i];
     }
-    return prod ;
+    return prod;
 }
 
 
 
-for(let i=0; i<=arr.length-1;i++){
-    if(arr[i]%2 == 0){
-        arr_even.push(arr[i])
-    }
-    else{
-        arr_odd.push(arr[i])
-    }
-}
-
-
-console.log("Even numbers",arr_even)
-console.log("Odd numbers",arr_odd)
+console.log("product of Even Indices",product(arr_indi));
 console.log("Sum of Even numbers: ", sum(arr_even))
-console.log("Product of Odd numbers:", product(arr_even))
 console.log("number of odd numbers" ,arr_odd.length)
